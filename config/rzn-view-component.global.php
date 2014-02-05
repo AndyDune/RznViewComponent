4acf279423f5adf6b052d5be31d177476bf667b1
Andrey Ryzhov <dune@rznlf.ru>
2014-02-05 14:41:55 +0400

    Add example congig file rzn-view-component.global.php

---
+++ config/rzn-view-component.global.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'rznviewcomponent' => array(
+        'cache_service' => 'cache_view_component',
+        'cache_adapter' => array(
+                                'name' => 'filesystem',
+                                'options' => array(
+                                    'ttl' => 3600,
+                                    'dirLevel' => 2,
+                                    'file_locking' => false,
+                                    'cacheDir' => 'data/cache',
+                                    'dirPermission' => 0755,
+                                    'filePermission' => 0666,
+                                ),
+                            ),
+        'cache_remove_item_key' => 'slay_component_cache', // add parameter into request string to clear cache with current page
+        'cache_allow'   => false, // check cache adapter options and set true to enable component cache
+        'view_script_prefix' => '',
+        'use_result_object' => false // set TRUE if you need retrieve extra data from component
+    )
+);
